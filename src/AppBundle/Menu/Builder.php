ef94e7737e7661b9614e586b47757ee30b78df9c
AnastasiaShimko <anastasiaishimko@gmail.com>
2017-06-14 21:42:02 +0300

    Top menu (KnpMenuBundle), changing password form style.

--- src/AppBundle/Menu/Builder.php
+++ src/AppBundle/Menu/Builder.php
@@ -8,32 +8,20 @@ use Symfony\Component\DependencyInjection\ContainerAwareTrait;
 
 class Builder implements ContainerAwareInterface
 {
-use ContainerAwareTrait;
+    use ContainerAwareTrait;
 
-public function mainMenu(FactoryInterface $factory, array $options)
-{
-$menu = $factory->createItem('root');
+    public function mainMenu(FactoryInterface $factory, array $options)
+    {
+        $menu = $factory->createItem('root');
+        $menu->addChild('Log out', array('route' => 'logout'));
+        $menu->addChild('Profile', array('route' => 'main'));
+        $menu['Profile']->addChild('New password', array('route' => 'password_change'));
+        $menu['Profile']->addChild('Change password', array('route' => 'change_user'));
+        $menu->addChild('Add articles', array('route' => 'new_article'));
+        $menu->addChild('Users', array('route' => 'account_control'));
+        $menu->addChild('Home', array('route' => 'main'));
 
-$menu->addChild('Home', array('route' => 'main'));
-$menu->addChild('Home', array('route' => 'main'));
-
-// access services from the container!
-//$em = $this->container->get('doctrine')->getManager();
-// findMostRecent and Blog are just imaginary examples
-//$blog = $em->getRepository('AppBundle:Blog')->findMostRecent();
-
-//$menu->addChild('Latest Blog Post', array(
-//'route' => 'blog_show',
-//'routeParameters' => array('id' => $blog->getId())
-//));
-
-// create another menu item
-//$menu->addChild('About Me', array('route' => 'about'));
-// you can also add sub level's to your menu's as follows
-//$menu['About Me']->addChild('Edit profile', array('route' => 'edit_profile'));
-
-// ... add more children
-
-return $menu;
+        return $menu;
+    }
 }
-}
+
