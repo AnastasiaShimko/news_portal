dddbce3c7e0482e47f3fae7feddfdd67cded807b
AnastasiaShimko <anastasiaishimko@gmail.com>
2017-06-18 19:40:37 +0300

    Base Symfony translation added.

--- src/AppBundle/Form/RegistrationForm.php
+++ src/AppBundle/Form/RegistrationForm.php
@@ -9,6 +9,8 @@ use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\HttpFoundation\Response;
+
 
 class RegistrationForm extends AbstractType
 {
